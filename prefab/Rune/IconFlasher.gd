16cb4c1c972c92902674479f341c22bc32e0db69
Fredrik Gustafsson <fredrikgson@hotmail.com>
2024-11-08 13:26:50 +0100

    Minor change to icon flasher

--- prefab/Rune/IconFlasher.gd
+++ prefab/Rune/IconFlasher.gd
@@ -2,10 +2,10 @@ extends Node2D
 class_name RuneIconFlasher
 
 @onready var sprite:Sprite2D = $Sprite2D
-@onready var text_container:Control = $Text
+@onready var spell_name_label:Label = $Text/Label
 @onready var anim:AnimationPlayer = $AnimationPlayer
 
 func flash_spell(spell:Spell):
-	text_container.get_node("Label").text = spell.name
+	spell_name_label.text = spell.name
 	sprite.texture = spell.icon
 	anim.play("flash")
